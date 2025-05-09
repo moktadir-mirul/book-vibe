c71e868bd099c529f9b28a21bb8e5853ea235a28
moktadir-mirul <moktadir.mirul@gmail.com>
2025-05-09 19:56:46 +0600

    some minor design change

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -3,12 +3,8 @@ import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
     const {id} = useParams();
-    console.log(typeof id);
-
     const allBooks = useLoaderData();
-    console.log(allBooks);
     const bookDetailsData = allBooks.find(book => book.bookId === Number(id));
-    console.log(bookDetailsData);
     const {bookName, author, rating, image, review, totalPages, yearOfPublishing, publisher} = bookDetailsData;
     return (
         <div className='w-11/12 border-b-2 border-gray-200 border-t-2 mx-auto py-8 flex justify-between items-start gap-7'>
@@ -16,11 +12,13 @@ const BookDetails = () => {
                 <img src={image} alt="" />
             </div>
             <div>
-                <h1>Book Name: {bookName}</h1>
-                <h1> Author: {author}</h1>
-                <h1>Review <br />{review}</h1>
-                <h1>{rating}{yearOfPublishing}{publisher}</h1>
-                <h1>Number of Pages: {totalPages}</h1>
+                <h1 className='font-bold text-2xl'>Book Name: {bookName}</h1>
+                <h1 className='font-bold text-lg'> Author: {author}</h1>
+                <h1><strong>Review</strong> <br />{review}</h1>
+                <h1><strong>Rating:</strong> {rating}</h1>
+                <h1><strong>Year of Publishing:</strong> {yearOfPublishing}</h1>
+                <h1><strong>Publisher: </strong>{publisher}</h1>
+                <h1 className='font-medium'><strong>Number of Pages:</strong> {totalPages}</h1>
                 <button className='btn btn-success '>Mark as Read</button>
                 <button className='btn btn-success m-2'>Add to WishList</button>
             </div>
---
+++ src/Utility/addToWishList.js
@@ -0,0 +1,4 @@
+const addToDB = () => {
+
+}
+export {addToDB}
